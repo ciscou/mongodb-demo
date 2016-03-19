ae0c507efde106dddf6274a99b1357b569894e2c
ciscou <francismpp@gmail.com>
2016-03-19 22:57:09 +0100

    nemeses calculator

---
+++ app/models/nemeses_calculator.rb
@@ -0,0 +1,37 @@
+class NemesesCalculator
+  def initialize(person_id, scope = :world)
+    @person, @scope = Person.find(person_id), scope
+  end
+
+  def nemeses
+    nemesis_ids = category_nemeses(SingleRank) & category_nemeses(SingleRank)
+    nemeses = Person.find(nemesis_ids)
+
+    nemeses.select do |nemesis|
+      case @scope
+      when :world
+        true
+      when :continent
+        nemesis.country.continent == @person.country.continent
+      when :country
+        nemesis.country_id == @person.country_id
+      end
+    end
+  end
+
+  private
+
+  def category_nemeses(category)
+    event_nemeses = category.where(person_id: @person.id).map do |rank|
+      category.where(
+        "event_id" => rank.event_id,
+        "#{@scope}_rank" => {
+          "$lt" => rank.send("#{@scope}_rank")
+        }).pluck(:person_id)
+    end
+
+    event_nemeses.inject do |a, e|
+      a &= e
+    end
+  end
+end
